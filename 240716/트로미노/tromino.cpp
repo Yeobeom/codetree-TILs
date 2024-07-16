8e422ce3e04fcf6e9abcff9ca4ae93a2d0dc5aee
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-07-16 22:48:16 +0900

    Title: 트로미노, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/트로미노/tromino.cpp
+++ 240716/트로미노/tromino.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 
 constexpr int MAX_N = 200;
-int vect[MAX_N][MAX_N];
+int vect[MAX_N + 3][MAX_N + 3];
 
 int main(int argc, char** argv)
 {
@@ -10,9 +10,9 @@ int main(int argc, char** argv)
 
     int n, m;
     std::cin >> n >> m;
-    for (int y = 0; y < n; ++y)
+    for (int y = 1; y <= n; ++y)
     {
-        for (int x = 0; x < m; ++x)
+        for (int x = 1; x <= m; ++x)
         {
             std::cin >> vect[y][x];
         }
@@ -20,45 +20,27 @@ int main(int argc, char** argv)
 
     int ret = 0;
 
-    for (int y = 0; y < n; ++y)
+    for (int y = 1; y <= n; ++y)
     {
-        for (int x = 0; x < m; ++x)
+        for (int x = 1; x <= m; ++x)
         {
-            if (y - 1 >= 0 && x - 1 >= 0)
-            {
-                int tmp = vect[y][x] + vect[y - 1][x] + vect[y][x - 1];
-                ret = ret < tmp ? tmp : ret;
-            }
+            int tmp = vect[y][x] + vect[y - 1][x] + vect[y][x - 1];
+            ret = ret < tmp ? tmp : ret;
 
-            if (y - 1 >= 0 && x + 1 < m)
-            {
-                int tmp = vect[y][x] + vect[y - 1][x] + vect[y][x + 1];
-                ret = ret < tmp ? tmp : ret;
-            }
+            tmp = vect[y][x] + vect[y - 1][x] + vect[y][x + 1];
+            ret = ret < tmp ? tmp : ret;
 
-            if (y + 1 < n && x - 1 >= 0)
-            {
-                int tmp = vect[y][x] + vect[y + 1][x] + vect[y][x - 1];
-                ret = ret < tmp ? tmp : ret;
-            }
+            tmp = vect[y][x] + vect[y + 1][x] + vect[y][x - 1];
+            ret = ret < tmp ? tmp : ret;
 
-            if (y - 1 >= 0 && x + 1 < m)
-            {
-                int tmp = vect[y][x] + vect[y + 1][x] + vect[y][x + 1];
-                ret = ret < tmp ? tmp : ret;
-            }
+            tmp = vect[y][x] + vect[y + 1][x] + vect[y][x + 1];
+            ret = ret < tmp ? tmp : ret;
 
-            if (y + 2 < n)
-            {
-                int tmp = vect[y][x] + vect[y + 1][x] + vect[y + 2][x];
-                ret = ret < tmp ? tmp : ret;
-            }
+            tmp = vect[y][x] + vect[y + 1][x] + vect[y + 2][x];
+            ret = ret < tmp ? tmp : ret;
 
-            if (x + 2 < m)
-            {
-                int tmp = vect[y][x] + vect[y][x + 1] + vect[y][x + 2];
-                ret = ret < tmp ? tmp : ret;
-            }
+            tmp = vect[y][x] + vect[y][x + 1] + vect[y][x + 2];
+            ret = ret < tmp ? tmp : ret;
         }
     }
 
