33088acd3cb5482844d80bdec67f1e140c96c717
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-07-16 22:27:58 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240716/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+
+constexpr int MAX_N = 100;
+int vect[MAX_N][MAX_N];
+
+int main(int argc, char** argv)
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(NULL); std::cout.tie(NULL);
+
+    int n, m;
+    std::cin >> n >> m;
+
+    for (int y = 0; y < n; ++y)
+    {
+        for (int x = 0; x < n; ++x)
+        {
+            std::cin >> vect[y][x];
+        }
+    }
+
+    int ans = 0;
+
+    for (int y = 0; y < n; ++y)
+    {
+        int prev = vect[y][0];
+        int cnt = 1;
+        for (int x = 1; x < n; ++x)
+        {
+            if (prev != vect[y][x]) cnt = 1;
+            else
+            {
+                if (++cnt >= m)
+                {
+                    ans++;
+                    break;
+                }
+            }
+
+            prev = vect[y][x];
+        }
+    }
+
+    for (int x = 0; x < n; ++x)
+    {
+        int prev = vect[0][x];
+        int cnt = 1;
+        for (int y = 1; y < n; ++y)
+        {
+            if (prev != vect[y][x]) cnt = 1;
+            else
+            {
+                if (++cnt >= m)
+                {
+                    ans++;
+                    break;
+                }
+            }
+            prev = vect[y][x];
+        }
+    }
+
+    std::cout << ans;
+
+    return EXIT_SUCCESS;
+}
