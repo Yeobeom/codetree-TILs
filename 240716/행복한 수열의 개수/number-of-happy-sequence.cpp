e5c4f01d6b1f7d96b8d8e56ff9315773e40c6502
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-07-16 22:30:04 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240716/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240716/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -21,44 +21,53 @@ int main(int argc, char** argv)
 
     int ans = 0;
 
-    for (int y = 0; y < n; ++y)
+    if (m == 1)
     {
-        int prev = vect[y][0];
-        int cnt = 1;
-        for (int x = 1; x < n; ++x)
+        ans = 2 * n;
+    }
+    else
+    {
+        for (int y = 0; y < n; ++y)
         {
-            if (prev != vect[y][x]) cnt = 1;
-            else
+            int prev = vect[y][0];
+            int cnt = 1;
+            for (int x = 1; x < n; ++x)
             {
-                if (++cnt >= m)
+                if (prev != vect[y][x]) cnt = 1;
+                else
                 {
-                    ans++;
-                    break;
+                    if (++cnt >= m)
+                    {
+                        ans++;
+                        break;
+                    }
                 }
-            }
 
-            prev = vect[y][x];
+                prev = vect[y][x];
+            }
+        }
+
+        for (int x = 0; x < n; ++x)
+        {
+            int prev = vect[0][x];
+            int cnt = 1;
+            for (int y = 1; y < n; ++y)
+            {
+                if (prev != vect[y][x]) cnt = 1;
+                else
+                {
+                    if (++cnt >= m)
+                    {
+                        ans++;
+                        break;
+                    }
+                }
+                prev = vect[y][x];
+            }
         }
     }
 
-    for (int x = 0; x < n; ++x)
-    {
-        int prev = vect[0][x];
-        int cnt = 1;
-        for (int y = 1; y < n; ++y)
-        {
-            if (prev != vect[y][x]) cnt = 1;
-            else
-            {
-                if (++cnt >= m)
-                {
-                    ans++;
-                    break;
-                }
-            }
-            prev = vect[y][x];
-        }
-    }
+
 
     std::cout << ans;
 
