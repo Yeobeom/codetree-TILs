e69032065531d47c2b5776b8f1a4dfd3b65cac3f
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-07-20 14:37:41 +0900

    Title: 가장 큰 증가 부분 수열, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240720/가장 큰 증가 부분 수열/largest-increasing-subsequence.cpp
+++ 240720/가장 큰 증가 부분 수열/largest-increasing-subsequence.cpp
@@ -2,8 +2,8 @@
 
 constexpr int MAX = 1000;
 
-std::pair<int,int> seq[MAX + 1];
-int dp[MAX + 1][2];
+int seq[MAX + 1];
+int dp[MAX + 1];
 
 struct Compare
 {
@@ -21,14 +21,11 @@ int main(int argc, char** argv)
 
 	int n;
 	std::cin >> n;
-	seq[0] = { 0,0 };
+	seq[0] = 0;
 	for (int i = 1; i <= n; i++)
 	{
-		std::cin >> seq[i].first;
-		seq[i].second = i;
+		std::cin >> seq[i];
 	}
-
-	std::sort(seq, seq + n + 1, cmp);
 
 	int maxDist = 0;
 	for (int i = 1; i <= n; i++)
@@ -36,27 +33,20 @@ int main(int argc, char** argv)
 		for (int j = i - 1; j >= 0; --j)
 		{
 			// 만일 현재 인덱스 보다 이전 인덱스가 작다면
-			if (seq[i].second > seq[j].second)
+			if (seq[i] > seq[j])
 			{
-				if (dp[i][0] < dp[j][0] + 1)
+				if (dp[i] < dp[j] + seq[i])
 				{
-					dp[i][0] = dp[j][0] + 1;
-					dp[i][1] = seq[i].first + dp[j][1];
+					dp[i] = dp[j] + seq[i];
 				}
-				if (maxDist < dp[i][0]) maxDist = dp[i][0];
 			}
 		}
 	}
 	int maxSum = -1;
 	for (int i = 1; i <= n; i++)
 	{
-		if (dp[i][0] == maxDist)
-		{
-			if (dp[i][1] > maxSum) maxSum = dp[i][1];
-		}
+		if (maxSum < dp[i]) maxSum = dp[i];
 	}
-
-
 
 	std::cout << maxSum;
 
