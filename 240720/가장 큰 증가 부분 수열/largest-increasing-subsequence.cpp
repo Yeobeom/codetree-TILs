b9389f25e1eda9f75821af9bc9aaee8be5ca4064
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-07-20 14:34:39 +0900

    Title: 가장 큰 증가 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240720/가장 큰 증가 부분 수열/largest-increasing-subsequence.cpp
+++ 240720/가장 큰 증가 부분 수열/largest-increasing-subsequence.cpp
@@ -2,7 +2,7 @@
 
 constexpr int MAX = 1000;
 
-std::pair<int,int> seq[MAX];
+std::pair<int,int> seq[MAX + 1];
 int dp[MAX + 1][2];
 
 struct Compare
@@ -12,7 +12,6 @@ struct Compare
 		if (l.first != r.first) return l.first < r.first;
 		return l.second > r.second;
 	}
-
 }cmp;
 
 int main(int argc, char** argv)
@@ -22,51 +21,42 @@ int main(int argc, char** argv)
 
 	int n;
 	std::cin >> n;
-	for (int i = 0; i < n; i++)
+	seq[0] = { 0,0 };
+	for (int i = 1; i <= n; i++)
 	{
 		std::cin >> seq[i].first;
 		seq[i].second = i;
 	}
 
-	std::sort(seq, seq + n, cmp);
+	std::sort(seq, seq + n + 1, cmp);
 
 	int maxDist = 0;
-	for (int i = 0; i < n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		dp[i][0] = 1;
-		dp[i][1] = -1;
 		for (int j = i - 1; j >= 0; --j)
 		{
 			// 만일 현재 인덱스 보다 이전 인덱스가 작다면
 			if (seq[i].second > seq[j].second)
 			{
-				// 만일 현재 인덱스와 이전 인덱스를 추가한 길이가 현재 인덱스를 포함한 다른 길이보다 길다면
 				if (dp[i][0] < dp[j][0] + 1)
 				{
 					dp[i][0] = dp[j][0] + 1;
-					if (maxDist < dp[i][0]) maxDist = dp[i][0];
-					dp[i][1] = j;
+					dp[i][1] = seq[i].first + dp[j][1];
 				}
+				if (maxDist < dp[i][0]) maxDist = dp[i][0];
 			}
 		}
 	}
-
 	int maxSum = -1;
-
-	for (int i = n - 1; i >= 0; --i)
+	for (int i = 1; i <= n; i++)
 	{
 		if (dp[i][0] == maxDist)
 		{
-			int next = dp[i][1];
-			int sum = seq[i].first;
-			while (next > -1)
-			{
-				sum += seq[next].first;
-				next = dp[next][1];
-			}
-			if (maxSum < sum) maxSum = sum;
+			if (dp[i][1] > maxSum) maxSum = dp[i][1];
 		}
 	}
+
+
 
 	std::cout << maxSum;
 
