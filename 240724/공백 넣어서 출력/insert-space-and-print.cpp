574bfd03489009a3d4819dc8529cda7e2ce952df
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-07-24 22:53:20 +0900

    Title: 공백 넣어서 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240724/공백 넣어서 출력/insert-space-and-print.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    std::cout <<
+    "score          subject          name\n\n  100             math           sam\n   88          science          paul\n   96         language          jack";
+    return 0;
+}
