d873dc58368f96e594cee6e7ef52be11bea1b779
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-08-01 23:51:42 +0900

    Title: 1차원 젠가, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240801/1차원 젠가/jenga-1d.cpp
+++ 240801/1차원 젠가/jenga-1d.cpp
@@ -6,14 +6,13 @@ int tmp[10000];
 
 int main() {
     int n;
-    cin >>n;
-    for(int i = 0; i < n; i++) cin >> blocks[i];
+    cin >> n;
+    for (int i = 0; i < n; ++i) cin >> blocks[i];
 
     int s,e;
     cin >> s >> e;
-    s = n - s - 1;
-    e = n - e - 1;
-    for(int i = e; i <= s; i++) blocks[i] = -1;
+    --s, --e;
+    for(int i = s; i <= e; i++) blocks[i] = -1;
     int tmp_idx = 0;
     for(int i = 0; i < n; i++) 
     {
@@ -23,9 +22,8 @@ int main() {
     n = tmp_idx;
 
     cin >> s >> e;
-    s = n - s;
-    e = n - e;
-    for(int i = e; i <= s; i++) blocks[i] = -1;
+    --s, --e;
+    for(int i = s; i <= e; i++) blocks[i] = -1;
     tmp_idx = 0;
     for(int i = 0; i < n; i++) 
     {
