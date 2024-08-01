120ad1d4a1eb237e8519785b07acb8d95ccb20b6
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-08-01 23:40:16 +0900

    Title: 최단 Run Length 인코딩, Time: 16ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240801/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+int blocks[10000];
+int tmp[10000];
+
+int main() {
+    int n;
+    cin >>n;
+    for(int i = 0; i < n; i++) cin >> blocks[i];
+
+    int s,e;
+    cin >> s >> e;
+    s = n - s - 1;
+    e = n - e - 1;
+    for(int i = e; i <= s; i++) blocks[i] = -1;
+    int tmp_idx = 0;
+    for(int i = 0; i < n; i++) 
+    {
+        if(blocks[i] != -1) tmp[tmp_idx++] = blocks[i];
+    }
+    for(int i = 0; i < tmp_idx; i++) blocks[i] = tmp[i];
+    n = tmp_idx;
+
+    cin >> s >> e;
+    s = n - s - 1;
+    e = n - e - 1;
+    for(int i = e; i <= s; i++) blocks[i] = -1;
+    tmp_idx = 0;
+    for(int i = 0; i < n; i++) 
+    {
+        if(blocks[i] != -1) tmp[tmp_idx++] = blocks[i];
+    }
+    for(int i = 0; i < tmp_idx; i++) blocks[i] = tmp[i];
+    n = tmp_idx;
+
+    std::cout << n << '\n';
+    for(int i = 0; i < n; i++) cout << blocks[i] << '\n';
+
+
+    return 0;
+}
