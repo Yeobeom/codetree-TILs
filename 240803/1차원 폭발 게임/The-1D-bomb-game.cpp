5059b9038360487a147af1a880528bca950439b1
윤여범 <75617688+Yeobeom@users.noreply.github.com>
2024-08-03 00:28:57 +0900

    Title: 1차원 폭발 게임, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240803/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+#include<list>
+
+int N, M;
+int bombs[201];
+int tmp[201];
+
+bool Boom()
+{
+	bool isBoom = false;
+	int prev = bombs[0];
+	int cnt = 1;
+	int tmpN = N;
+	for (int i = 1; i < N; i++)
+	{
+		if (prev == bombs[i]) cnt++;
+		else
+		{
+			if (cnt >= M)
+			{
+				tmpN -= cnt;
+				isBoom = true;
+				for (int j = 1; j <= cnt; j++) bombs[i - j] = 0;
+				cnt = 1;
+			}
+		}
+		prev = bombs[i];
+	}
+	if (cnt >= M)
+	{
+		isBoom = true;
+		for (int j = 1; j <= cnt; j++) bombs[N - j] = 0;
+		tmpN -= cnt;
+	}
+	int tmpIdx = 0;
+	for (int i = 0; i < N; i++)
+	{
+		if (bombs[i] == 0) continue;
+		tmp[tmpIdx++] = bombs[i];
+	}
+	
+	for (int i = 0; i < N; i++) bombs[i] = 0;
+	for (int i = 0; i < tmpIdx; i++) bombs[i] = tmp[i];
+	N = tmpN;
+	return isBoom;
+}
+
+int main(int argc, char** argv)
+{
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL); std::cout.tie(NULL);
+
+	std::cin >> N >> M;
+	for (int i = 0; i < N; i++) std::cin >> bombs[i];
+	while (Boom());
+
+	std::cout << N << '\n';
+	for (int i = 0; i < N; i++) std::cout << bombs[i] << '\n';
+
+	return EXIT_SUCCESS;
+}
